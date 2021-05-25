3323fd9f96f8d7c136eef4661795dc5e39a030e7
Mary Toth <maryarylynn@gmail.com>
2021-05-25 13:53:50 -0400

    improved css

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,31 @@ import React, { Component } from 'react'
 export class App extends Component {
   render() {
     return (
-      <div className="gameboard">
-        <header>Minesweeper</header>
+      <div className="minesweeper">
+        <header>
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+          </style>
+          <h1>minesweeper</h1>
+        </header>
+
         <section>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
-            <li>11</li>
-            <li>12</li>
-            <li>13</li>
-            <li>14</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
             <li></li>
             <li></li>
             <li></li>
